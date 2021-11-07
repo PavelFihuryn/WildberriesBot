883a64286f95af9d3c8c8690db8f37a2548a6536
PavelFihuryn <pavelfihuryn@gmail.com>
2021-11-07 20:39:23 +0300

    create bot

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     from handlers import dp, send_to_admin  
     
     loop = asyncio.get_event_loop()
-    # executor.start_polling(dp, on_startup=send_to_admin)
+    executor.start_polling(dp, on_startup=send_to_admin)
     executor.start_webhook(listen="0.0.0.0",
                            port=int(PORT),
                            url_path=BOT_TOKEN)
